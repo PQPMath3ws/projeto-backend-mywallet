4f91e26f3d6dbdb644828d8306e5c2f1ff1e6034
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-19 11:53:15 -0300

    Adicionados schemas da aplicação

---
+++ src/schemas/movimentations.js
@@ -0,0 +1,25 @@
+import Joi from "joi";
+
+const MovimentationsSchema = Joi.object({
+    value: Joi.number().precision(2).required(),
+    description: Joi.string().min(6).max(50).required(),
+    userId: Joi.string().min(24).max(24).regex(/^[a-f0-9]{24}$/).required(),
+    created: Joi.date().required(),
+});
+
+async function validateMovimentation(movimentation) {
+    try {
+        await MovimentationsSchema.validateAsync(movimentation);
+        return {
+            status: "ok",
+            message: "movimentation validated successfully",
+        };
+    } catch (error) {
+        return {
+            status: "error",
+            message: error.details[0].message,
+        };
+    }
+}
+
+export { validateMovimentation };
