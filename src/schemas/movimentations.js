9aa954f38642ab8c50f10627f4f9d91f98ac1e4f
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-20 11:24:07 -0300

    Adicionado campo faltoso de tipo de movimentação

--- src/schemas/movimentations.js
+++ src/schemas/movimentations.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 const MovimentationsSchema = Joi.object({
     value: Joi.number().precision(2).required(),
     description: Joi.string().min(6).max(50).required(),
+    isEntry: Joi.boolean().required(),
     userId: Joi.string().min(24).max(24).regex(/^[a-f0-9]{24}$/).required(),
     created: Joi.date().required(),
 });
